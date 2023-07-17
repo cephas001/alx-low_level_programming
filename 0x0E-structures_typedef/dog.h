4c644f1ae39548acabb8a07f7d3880b1169d1747
Cephas <schoolthingsforpeter@gmail.com>
2023-07-17 06:54:51 -0700

    File created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Describes Dog
+ * @name: First member
+ * @age: Second member 
+ * @owner: Third member
+ *
+ * Description: This structure gives the properties of a dog
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
